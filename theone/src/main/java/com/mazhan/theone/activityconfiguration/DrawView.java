8175959cf367edbeff51e8e2dcb3082f06910b83
dragonfly <longyuan02@qq.com>
2017-04-21 15:11:56 +0800

    change costom view

--- theone/src/main/java/com/mazhan/theone/activityconfiguration/DrawView.java
+++ theone/src/main/java/com/mazhan/theone/activityconfiguration/DrawView.java
@@ -4,7 +4,19 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.transition.Transition;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import com.mazhan.theone.R;
 
 /**
  * Created by dragonfly on 2017/4/20.
@@ -15,16 +27,27 @@ public class DrawView extends TextView {
     private Bitmap bitmap1;
     private int widthMeasure;
     private int heightMeasure;
+    private Paint mPaint = null;
+    private LinearGradient mLinearGradient = null;
+    private Matrix mGradientMatrix;
+    private int mViewWidth = 0;
+    private int mTranslate = 0;
+    private boolean mAnimating = true;
 
-    public DrawView(Context context) {
-        super(context);
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+//        mPaint = getPaint();
+//        mLinearGradient = new LinearGradient(0, 0, widthMeasure, 0, new int[]{Color.YELLOW, Color.GREEN, Color.WHITE}, null, Shader.TileMode.CLAMP);
+//        mPaint.setShader(mLinearGradient);
     }
 
-//    @Override
-//    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-//        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-//        setMeasuredDimension(measureWidth(measureWidth(widthMeasureSpec)), measureHeight(heightMeasureSpec));
-//    }
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        widthMeasure = widthMeasureSpec;
+        heightMeasure = heightMeasureSpec;
+        setMeasuredDimension(measureWidth(measureWidth(widthMeasureSpec)), measureHeight(heightMeasureSpec));
+    }
 
     private int measureWidth(int measureSpec) {
         int result = 0;
@@ -56,18 +79,56 @@ public class DrawView extends TextView {
         return result;
     }
 
+    /**
+     * 闪烁动画
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(-mViewWidth, 0, 0, 0,
+                        new int[] { 0x33ff0000, 0xffff00, 0x33ff0000 },
+                        new float[] { 0, 0.8f, 1 }, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
 //        Paint mPaint1 = new Paint();
-//        mPaint1.setColor(0xFF0000);
+//        mPaint1.setColor(getResources().getColor(R.color.colorAccent));
 //        mPaint1.setStyle(Paint.Style.FILL);
 //        Paint mPaint2 = new Paint();
-//        mPaint1.setColor(0xFFFF00);
-//        mPaint1.setStyle(Paint.Style.FILL);
+//        mPaint2.setColor(getResources().getColor(R.color.colorPrimaryDark));
+//        mPaint2.setStyle(Paint.Style.FILL);
 //        canvas.drawRect(0, 0, widthMeasure, heightMeasure, mPaint1);
 //        canvas.drawRect(10, 10, widthMeasure - 10, heightMeasure - 10, mPaint2);
+//        canvas.save();
+//        super.onDraw(canvas);
+//        canvas.restore();-----------------------------------------------------------------------------------
         super.onDraw(canvas);
-//        canvas.drawBitmap(bitmap1,0,0,null);
+/**
+ * 闪烁
+ */
+        if (mAnimating && mGradientMatrix != null) {
+            //每一次运动的递增值
+            mTranslate += mViewWidth / 10;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(50);
+        }
     }
 }
 
---
+++ theone/src/main/java/com/mazhan/theone/viewgroup/MyTextView.java
@@ -0,0 +1,58 @@
+package com.mazhan.theone.viewgroup;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by dragonfly on 2017/4/21.
+ */
+
+@SuppressLint("AppCompatCustomView")
+public class MyTextView extends TextView {
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private int mTranslate = 0;
+    private boolean mAnimating = true;
+    //自定义的View 必须要重写两个或者三个参数的构造方法，才能在Eclipse视图浏览中查看效果。
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(-mViewWidth, 0, 0, 0,
+                        new int[] { 0x33ffffff, 0xffffffff, 0x33ffffff },
+                        new float[] { 0, 0.5f, 1 }, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mAnimating && mGradientMatrix != null) {
+            //每一次运动的递增值
+            mTranslate += mViewWidth / 10;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(50);
+        }
+    }
+}
